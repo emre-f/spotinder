092af615deedbe2de3d3efa8a63237a8e38e9513
emre-f <97183433+emre-f@users.noreply.github.com>
2022-06-01 16:17:28 -0400

    back to desktop

--- routes/playlistMatch.js
+++ routes/playlistMatch.js
@@ -74,33 +74,121 @@ router.route('/validate')
             res.render('playlistMatch', { accessToken, accessTokenPresent, error })
         }
 
-        printer();
+        playlistOneGenres = await getTopGenres(accessToken, playlistOneData)
+        playlistTwoGenres = await getTopGenres(accessToken, playlistTwoData)
 
-        res.render('playlistMatchResults', { accessToken, accessTokenPresent, playlistOneData, playlistTwoData })
+        res.render('playlistMatchResults', { 
+            accessToken, 
+            accessTokenPresent, 
+            playlistOneData, 
+            playlistTwoData,
+            playlistOneGenres,
+            playlistTwoGenres
+        })
     })
 
 module.exports = router;
 
 // From the top 100 songs, get artists (with counts, to send less requests). Then get genres for all artists, multiply with count and return result.
-function getTopGenres (accessToken, playlistData) {
-    console.log("hi");
+async function getTopGenres (accessToken, playlistData) {
+    console.log("getTopGenres() called");
+
+    // Get all the songs (if tracks.length >100)
+    var tracks = playlistData.tracks.items
+    var nextLink = playlistData.tracks.next; // if not null, more songs
+
+    while(nextLink !== null) {
+        console.log("next link: ", nextLink)
+        console.log("More songs, calling again...")
+        // Get next 100 songs
+        try {
+            axiosResponse = await axios.get(nextLink, { 
+                headers: {
+                    'Content-Type': 'application-json',
+                    'Authorization': `Bearer ${accessToken}`
+                }});
+        } catch (e) {
+            console.log("Caught an error getting extra songs")
+            console.log(e.response)
+    
+            var getNextSongsFailed = true;
+        } finally {
+            if (!getNextSongsFailed) { 
+                tracks.push.apply(tracks, axiosResponse.items)
+                nextLink = axiosResponse.next;
+            } else {
+                nextLink = null;
+            }
+        }
+    }
+
+    console.log("Total song count: ", tracks.length);
+
+    // Get a dictionary of artists and their counts
+    let artistIds = {};
+
+    // ARTISTS AND COUNTS
+    for (let i = 0; i < playlistData.tracks.items.length; i++) {
+        // console.log(playlistData.tracks.items[i].track.artists[0].name)
+        let artistId = playlistData.tracks.items[i].track.artists[0].id;
+
+        artistIds[artistId] = (artistIds[artistId] || 0) + 1;
+    }
+
+    // create 1 string of all ids, separated by commas
+    var ids = "";
+    for (let i = 0; i < Object.keys(artistIds).length; i++) {
+        let artistId = Object.keys(artistIds)[i];
+
+        if (i === 0) {
+            ids += artistId;
+            continue
+        }
+        
+        ids += "," + artistId
+    }
+
+    // GET ALL ARTISTS, GO THROUGH AND ADD THEIR GENRES
     try {
-        axiosResponse = await axios.get(`https://api.spotify.com/v1/playlists/${playlistOneId}`, { 
+        axiosResponse = await axios.get(`https://api.spotify.com/v1/artists?ids=${ids}`, { 
             headers: {
                 'Content-Type': 'application-json',
                 'Authorization': `Bearer ${accessToken}`
             }});
     } catch (e) {
-        console.log("Caught an error for playlist 1")
+        console.log("Caught an error getting all artists")
         console.log(e.response.data.error)
 
-        var playlistOneFailed = true;
+        var getAllArtistsFailed = true;
     } finally {
-        if (!playlistOneFailed) { 
-            console.log("Playlist 1 is valid, name: ", axiosResponse.data.name) 
-            var playlistOneData = axiosResponse.data;
+        if (!getAllArtistsFailed) { 
+            var allArtists = axiosResponse.data.artists;
         }
     }
+
+    let allGenres = {};
+
+    for (let i = 0; i < allArtists.length; i++) {
+        let multiplier = artistIds[allArtists[i].id]; // how many songs they had?
+
+        for (let j = 0; j < allArtists[i].genres.length; j++) {
+            let genre = allArtists[i].genres[j];
+            allGenres[genre] = (allGenres[genre] || 0) + (1 * multiplier);
+        }
+    }
+
+    // Sort the genres and return the top ones
+    let genresArray = []
+    for (let i = 0; i < Object.keys(allGenres).length; i++) {
+        genresArray.push([Object.keys(allGenres)[i], allGenres[Object.keys(allGenres)[i]]])
+    }
+
+    genresArray.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    
+
+    return genresArray
 }
 
 // Old way to get axios get request
